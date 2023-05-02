4f7939b1feeaba5e26d8e5de59fdccb656444f68
AMOUCHAL Moustapha <amouchaldev@gmail.com>
2023-05-02 09:58:01 +0100

    fix projects images url

--- db/projects.js
+++ db/projects.js
@@ -1,30 +1,29 @@
 export const projects = [
     {
-      name: 'TMDB',
+      name: "TMDB",
       description: "TMDB is a user-friendly website that allows you to search for movies and TV shows, and add them to your favorite, watchlist, or already-watched lists.",
-      image: "https://lh3.googleusercontent.com/pw/AJFCJaVGo2d6GA4gUl-JMkremlOwxlSqME78RGfjgnP9F64yPX_4fzIxqF_pzDP30fOrpJUWqmkeZ1V1O6l85Y-O1Naa2zRTC5xm3TT90LPkAJc4QLJsIBuYUAxYVWYKf9Z9H9JWkoj7HI1uAk6dcahVMubDXhG5BXlCZ3AXddj7-BvTo-IDHiB73G0nrTI2ipNiLz0rFFpPU-wnRp668-1coaS0kwiog0DucmsfnPCVFOsA63U4uK_XTL3qcNtLdxu_g9U0e27p9BL7RKmeJa-dCl5xZ_-dh9DWxqgGdTn3Qoo1n4Da_m1LnOp4CUaea9WijXqximx-dQGrLDwsSqVnJp5Q00SkxF9B9A9cqfIYC6Qn2-Apm_z5y9cBb3MYqodLAyxD1KeSTEvvyfkheh7zzl0F_u4frMHlaQLokQ9zGCWcOP3TYN_xjfG70NDYWP_IDbHtApH8D-OxUcsZQlc6iShC7qjDDw-3fSL9Ku4jusiUCAT3OXci1p9CaWBfqt9DUmVyJP3XkbKMVZtQsjMFtPwXiig9U9OLpndAqc5r1wGFWY-Adi6YH9YgKeb2wLV4VwbYfs0gcq8E8aweSNcQqgruA8UQvrJSSREcI74QfTcjjiVXsKys0vHVyHZ4V0yf31U1Tfa-UsYPhLqt6PbtWdcbKT3XMQnCcMsGpQ1oEKM3HkhRvV6r3IFqVRfT1R59aR8jqPE_2sneEW4VVzP6GGOynD4BCUw-gLYo6miRa0BNESqK6HKmRrq-EGTJyUPhOauME04g9fkFxK2Jw2neKUgyURe7VioclnlC-YGrvqxzXVW8MkSG43JjqYfZMHHdAD4OcK2qJ7YKCYXfS0y-cA24unizGoacts7j9l34SlxpwJXJOsTxvxDXGFSPKcuEqA4mAeAlAYg8m87Z2FVHCzoqtEcfuiYFJcxLtgkQyWZZIL8PNYmEiZeb48j8MQ=w1862-h1022-s-no?authuser=1",
-      github: 'https://github.com/amouchaldev/tmdb',
+      image: "https://h.top4top.io/p_2678ix5ml4.png",
+      github: "https://github.com/amouchaldev/tmdb",
       stack: ['laravel', 'React']
     },
     {
-      name: 'netUS',
-      description: 'This project is a web application built with Laravel and MySQL that allows users to browse and interact with communities and their respective posts.',
-      image: "https://lh3.googleusercontent.com/pw/AJFCJaUcIaSbSlv8NDhcJYCeJptYxZffhfFkwIYm_iges3gCUq9ev2PkCkMyUwKpMIs1k5ud_wFr0yvAM2rYPXwdbNq69chQqLGsgOKfaF6gobgD8rnZ7RMpRlyVzLIccUJTHAlep5LnSIi1inbsfKK_C6eMr0wASj2IOXR1LQMJSo-zBsjqhoSLxvqzD8o_E4FwCmuXaF0AesdgMa9Z4NM2rWsC2wDQRuGPq4CbslVWVf2ZMi9GfAcs-raqocfs1_azfIM0La-Dsyn-BgKHomQYbkjy5uQeAHXZWESorfuv4D5_nsJkUruwIGCcp2a1AeJsyoy4JaIoxY9MNB77kOdUkzVdk5tW8G9zu9Z-sIMpHUezdR58XvzfEGS6pq1WgVFpCCKp_EuHtogmphLAkjtiqZ2nBCGAc0YpNUoqbC7smp8PNHj7-UivfWFfZj-PLV6QAbEZvDA5SoSnqaggXX2i150VgkPis9qt2tXThYJOxLbFayzK9tDphyveFZJqg5ugCRakHsgZg9uw9LCP10xpURuE82Rdr4Sugn8DUvbXi0HfSgGE32UWA1tDV7M0Kw-_3IHfHfP9Zr53-6zhLz8WuauukV3w2cxKFfpGOVvYMSvPWgt3gzgZjjetprb1PIVrVgxEsWi1Z0NBoBrGJJVk7pkZpuG0TYmYlrzF-e1x6p4J7Dkvc4iJTdI-mBOj1maL-wuYaUEjPwJbAxPNemDfqe5xQ8wrpXQnTPo9Qj9s-z-8q6jDplSVE-vOK18bvJ2OitT2uPtYn_W2RG2dThv8OIjJvFe-OladK62y8DpRIdyXu29Z8ubHpHkRSJc3azYK5ex6u0myWylVRCVuDZT0Lc2c86i8uvNE-ui1tPBBS_9yzhUwvVdsWkk8Dn-pJmZL-fJeU69gyUT9HoqZ5zRV73gEUI9TPpteypODiqwXUBruxpGVkXTIfjbsTTZKBg=w1808-h1022-s-no?authuser=1",
-      github: 'https://github.com/amouchaldev/netUS',
+      name: "netUS",
+      description: "This project is a web application built with Laravel and MySQL that allows users to browse and interact with communities and their respective posts.",
+      image: "https://f.top4top.io/p_2678erimy2.png",
       stack: ['laravel', 'mySQL', 'javascript', 'bootstrap']
     },
     {
-      name: 'streamio',
-      description: 'watch, download and add to your favorite movies, tv shows and animes.',
-      image: "https://lh3.googleusercontent.com/pw/AJFCJaWLJFmhwa17yI9pwFA_S73NKA5oxP0Pn609PaWc7Uwii8E6BZREm4_HHiQpq833yT74mrzeDnpyxj0MVkPO3WAtUsuDdW2d8EmNQSYc_4BPba8MsuZJac9Q2Yg_cmbOmGa2mcl9jyr_cTcDp83XnR5YLZJIn-odDd5nB2pRy6UhZg1lxig95uKzRIdVVg8GPzhMb-IUpwROgJ5QR45yXVdhQSWtOnrouPBHrLYUFOebPOOfQw425O3ArpbOaDlzIAdHRqYkFEobPzIqDZN2XKrVwcV2i4UdDea1MBSknbZqXD1INZfXOc8AnIBq9lEc6Q9g32JOP-uWwzCjqpfV1QCRGCYLXQLbtdcyrPqvKokS8Yv24OYN-bAR8XDbzKSPwXNpnIwla1LlzDpYrkzumki8_3WyG8W8XYdXqI8MMQcYVzajEkeDnY3HmRpi0Bqx11hKCZ2iH1iIkheKHS45dM_dD6IUQFMTuWVho32NZG3qRlspfYHDPbmQy3DohNQu7l20bCXWjs8SapPt0iggSv2VsG4K09eXgQLAD3lWwKFveRddg7TyMXovyVNirHPgJ7C3iojDaK5qoeOyejQTTgmXKXWZR2nJn2rd0pIR9A9KCT50fVL2P6rEv-Nqye3V91-tBQ9pVLjZpc0pJ1YVfAThpJtym1JL3phuvoxv-mm2v-NIxrdqnR-oyYXEZxIvEiKUtstHaycS5rBczG8cNs-8b1S6BNtV21bD9qp3l_uomn2_KtEIXANf2VlIZRQEBkmfRylqljQ0Tjt_u0mqECybkfDSABca6TIo11NAWwa0ifJ2lfuNNUHWMNOrir_BSzMssBKYtBbG6o_3HCiIi63dOTDilgGaq7LrL1I5rSzFkAv0zLg164ccOU-7zRnmpb3W-2g4urnSQSd4dcV133vmzZ-QDGOySPFYvGF0AJzz8YTTIR-bN21LYJd7lw=w1431-h1022-s-no?authuser=1",
-      github: 'https://github.com/amouchaldev/stream-app-with-laravel',
+      name: "streamio",
+      description: "watch, download and add to your favorite movies, tv shows and animes.",
+      image: "https://g.top4top.io/p_2678x3lnd3.png",
+      github: "https://github.com/amouchaldev/stream-app-with-laravel",
       stack: ['laravel', 'mySQL', 'javascript', 'bootstrap']
     },
     {
-      name: 'kanban to do list',
-      description: 'This is a simple to-do list. The application features a Kanban-style layout with four sections To-Do, In Progress, Testing, and Completed.',
-      image: "https://lh3.googleusercontent.com/pw/AJFCJaX4VgQW0wV_A1X3jppvjxhBo4h5QeQOIaWiOqyIwKvGXNfk_LaE7XFBglbYcA6ZqUrtqugbxfZYjF3gFOnJGYzT-wq8DaSsrLQi4zMITg7GxqzFWk1clqASCwUZZ2UT1mi6nE5ZFDnA-WRPCUGvmJS8m_XJgespvL7NP1Dbtmy5uw7spDJ2oPjKETrO1E10Bzz58eCe1x0keyx489ruyISbKSOZTovXvsKSbrmBZb_SkqAb0Rv3zVsQIbObcdByI0bZ69QEsLFTBTss_dPpZTa28sWu76tIxUye8WOEiT2divdt6fju2TIARsm0CaICfBr6hTwo7FV5mIHxoDZjdHSmsCEbRhYZW7ohgeJ-CyVvw67dI3zTQGdVpQUYcj1Q8u9Cf1dIcZ8-019addJR829OI9jN56URAXvzoTR-Dmxg72b2UsE8_wYse97C1KsSWtH0FlgU60qusT6f0p5i0YAGBmgk0SjARpFOrNYE91Q02dVRdGszeBx_f9zXXHsVjbg-Hejg4ma4fdFIU5w8bHw7ciyj0g1YCA0H2uRI_WmBihCHlp37lcwXi5plulpKQP2-bSorp5elIkoeQ1jZWv-rqJuUON9BMP5Ph4mNBbKb1slJNI6SwaooXghVIgM2tsD717rGPgn6AI9UTiM-BZSoEkjMwS0zoLYmbq20cw-hurj4nRB20ZANgoa9Qhi2NcD38QVBN9lk5KjRsA3yHIJQbpdc6DQf6oUuLocHT5KDfWZVG0F-Mr-3TQUzvSqoqvxIztA17A8k64h60_eZEJzXREQF_4ObHhnoacYNJMzM2c7qkuZdv6EFHi7W4C2_A9_o1e4aM9pRCijQzF6JaFvd0SxhBLJhLWUebG0_bA4iWyU1Ra1vp-n9XOHZqXI2hqQwnR_pHRaPTWnO7PT3QIC53CZ6atYNBiCKfPmOqtnMuQICco6ppNH6UGE5oA=w1920-h968-s-no?authuser=1",
-      github: 'https://github.com/amouchaldev/Todo-List-Kanban',
+      name: "kanban to do list",
+      description: "This is a simple to-do list. The application features a Kanban-style layout with four sections To-Do, In Progress, Testing, and Completed.",
+      image: "https://e.top4top.io/p_2678mj5z01.png",
+      github: "https://github.com/amouchaldev/Todo-List-Kanban",
       stack: ['HTML', 'CSS', 'javascript']
     }
   ]
